5b9b0bcf8ad1ba3098cb003b228700b826ffd1eb
imblackdevil123 <60686904+imblackdevil123@users.noreply.github.com>
2021-02-12 19:42:53 +0545

    first commit

---
+++ android/app/src/main/kotlin/com/example/classifyNepaliRupee/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.classifyNepaliRupee
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:classifyNepaliRupee/splashscreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Nepali Money Classifier',
+      home: MySplash(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
